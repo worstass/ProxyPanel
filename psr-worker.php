43a550b2ccf46cfa660d6b33d3b623276c47dd1f
nobody@nowhere.com <alex@DESKTOP-N8L9HTE.localdomain>
2021-11-16 14:50:37 +0800

    add psr worker

--- psr-worker.php
+++ psr-worker.php
@@ -1,17 +1,31 @@
 <?php
-declare(strict_types=1);
 
-ini_set('display_errors', 'stderr');
-include 'vendor/autoload.php';
+require __DIR__ . "/vendor/autoload.php";
+
+use Symfony\Bridge\PsrHttpMessage\Factory\DiactorosFactory;
+use Symfony\Bridge\PsrHttpMessage\Factory\HttpFoundationFactory;
 
 $relay = new Spiral\Goridge\StreamRelay(STDIN, STDOUT);
 $psr7 = new Spiral\RoadRunner\PSR7Client(new Spiral\RoadRunner\Worker($relay));
 
+$app = require_once __DIR__ . '/bootstrap/app.php';
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
 while ($req = $psr7->acceptRequest()) {
     try {
-        $resp = new \Zend\Diactoros\Response();
-        $resp->getBody()->write('Hello, world!');
-        $psr7->respond($resp);
+        $httpFoundationFactory = new HttpFoundationFactory();
+        if (strpos($req->getHeaderLine("content-type"), "application/json") === 0) {
+            $body = $req->getBody();
+            $parsedBody = json_decode($body, true);
+            $req = $req->withParsedBody($parsedBody);
+        }
+
+        $symfonyReq = $httpFoundationFactory->createRequest($req);
+        $request = Illuminate\Http\Request::createFromBase($symfonyReq);
+        $response = $kernel->handle($request);
+        $psr7factory = new DiactorosFactory();
+        $psr7response = $psr7factory->createResponse($response);
+        $psr7->respond($psr7response);
     } catch (\Throwable $e) {
         $psr7->getWorker()->error((string)$e);
     }
